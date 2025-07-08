73520c2e5ff1f8130569c5a7613b46e4f9a628be
Sara Monteiro <saramonteiro2008@gmail.com>
2025-07-08 16:48:48 -0300

    changes

--- Aroma-e-Sabor/obj/Debug/net8.0/Aroma-e-Sabor.AssemblyInfo.cs
+++ Aroma-e-Sabor/obj/Debug/net8.0/Aroma-e-Sabor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aroma-e-Sabor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4f6800b611797f4e097346545187de703decb55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da290556d9ecd9ed0fb221df19f66e0590f0c07f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aroma-e-Sabor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aroma-e-Sabor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
