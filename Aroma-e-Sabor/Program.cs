da290556d9ecd9ed0fb221df19f66e0590f0c07f
Sara Monteiro <saramonteiro2008@gmail.com>
2025-07-08 16:45:07 -0300

    changes

--- Aroma-e-Sabor/Program.cs
+++ Aroma-e-Sabor/Program.cs
@@ -1,18 +1,8 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Hosting;
-
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 // Habilita o uso de arquivos estáticos da pasta wwwroot
 app.UseDefaultFiles(); // Procura automaticamente por index.html
 app.UseStaticFiles();
-
-// Opcional: responde com home.html se acessar "/" e não houver index.html
-app.MapGet("/", async context =>
-{
-    context.Response.ContentType = "text/html";
-    await context.Response.SendFileAsync("wwwroot/index.html");
-});
 
 app.Run();
--- Aroma-e-Sabor/obj/Debug/net8.0/Aroma-e-Sabor.AssemblyInfo.cs
+++ Aroma-e-Sabor/obj/Debug/net8.0/Aroma-e-Sabor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aroma-e-Sabor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70ba2725a7855b9a709b64d5e410cae2a0009078")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4f6800b611797f4e097346545187de703decb55")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aroma-e-Sabor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aroma-e-Sabor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
