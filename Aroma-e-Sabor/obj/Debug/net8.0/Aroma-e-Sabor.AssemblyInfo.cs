cdf657fcde7c0a88be93c797eadfff048c9e927f
Sara Monteiro <saramonteiro2008@gmail.com>
2025-08-21 16:43:55 -0300

    foto perfil

--- Aroma-e-Sabor/obj/Debug/net8.0/Aroma-e-Sabor.AssemblyInfo.cs
+++ Aroma-e-Sabor/obj/Debug/net8.0/Aroma-e-Sabor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aroma-e-Sabor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67ba6fc66cdd7b92b78185d837dcb460bcda8630")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2183bf028b9f0024fc23696f3389313dcdcd4fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aroma-e-Sabor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aroma-e-Sabor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
